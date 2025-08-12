936cc5a36848efb2efc1f61cd0e04a600437aaef
Taras Ishchuk <ishchuktaras@gmail.com>
2025-08-13 01:16:15 +0200

    feat: Implementace základu pro ABRA Flexi a napojení na uzávěrku a klienty

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'; 
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Aktivujeme globální validační pipe s povolenou transformací
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
 
+  // TATO CEDULKA POVOLÍ PŘÍSTUP Z JINÝCH ADRES (NAPŘ. Z NAŠEHO FRONTENDU)
+  app.enableCors();
+
+  // Tento řádek zajišťuje, že server poslouchá na všech síťových rozhraních
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
