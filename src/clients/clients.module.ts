b461d6d1c13f2dfa367c621c69740d19a18b4b28
Taras Ishchuk <ishchuktaras@gmail.com>
2025-09-07 18:20:39 +0200

    fix: Correct Dockerfile build order for Prisma

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { PohodaModule } from '../pohoda/pohoda.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PohodaModule],
   controllers: [ClientsController],
   providers: [ClientsService],
 })
