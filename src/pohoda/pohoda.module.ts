7a84884ebfb3dccd9ef8328aea40200a4077f8a4
Taras Ishchuk <ishchuktaras@gmail.com>
2025-09-15 21:15:51 +0200

    Fix: Explicitní import ConfigModule pro zajištění správného pořadí inicializace

--- src/pohoda/pohoda.module.ts
+++ src/pohoda/pohoda.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PohodaService } from './pohoda.service';
 import { HttpModule } from '@nestjs/axios';
-// OPRAVA: Odstranili jsme import ConfigModule, protože je globální
-// import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  // OPRAVA: Odstranili jsme ConfigModule z imports.
-  // PohodaService si díky globalitě najde ConfigService sama.
-  imports: [HttpModule],
+  
+  imports: [HttpModule, ConfigModule],
   providers: [PohodaService],
   exports: [PohodaService],
 })
 export class PohodaModule {}
-
-
